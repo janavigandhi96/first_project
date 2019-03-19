eacb3830b30a2963150267de64e04c0153b2c640
avinashdepasser <avinash@depasserinfotech.in>
2019-03-19 11:50:13 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,20 @@
+# odd even 
+
+# num=[1,2,3,4,5,6,7,8,9,10]
+# for nums in num:
+# 	if(nums%2==0):
+# 		print(nums, "is even")
+# 	else:
+# 		print(nums," is odd")
+
+
+# print largest number
+a=1
+b=2
+c=3
+if(a>b and a>c):
+	print(a,"is largest")
+elif b>a and b>c:
+	print(b,"is largest")
+else:
+	print(c ,"is largest")
